b09ad641a297d494b51e00be08d48000f2e848d5
uo250487@uniovi.es <My code repo 11>
2020-11-15 22:12:30 +0100

    Configuración de Django, implementación de las 2 views para la GUI web, y maquetado de la de selección de rover (la "simple")

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'mainApp/index.html')
+
+
+def monitor(request, rover_id):
+    return HttpResponse("<h1>Monitoring room for Rover ID: <br/><br/>" + rover_id + "</h1>")
--- verneServer/settings.py
+++ verneServer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fimzt0$-3mv%wnre)0)#-#6kc-838@xs%3uz!dgj50cf_jnk)5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainApp',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
